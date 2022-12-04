ccc66e4d4f224adbcc4132bb1b7674f35f03238e
Tim024 <tim.vanderlee@gmail.com>
2022-12-04 13:50:51 +0100

    Code refactor

--- utils.py
+++
@@ -1,7 +0,0 @@
-def read_data(day, example=False, sep='\n'):
-    if example:
-        ext = 'example'
-    else:
-        ext = 'input'
-    with open(f"./input/day{day}.{ext}") as f:
-        return f.read().split(sep)
