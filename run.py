9b36b2ce463b0e7f81f3bb176ea2398d4b887233
Tim van der Lee <tim.vanderlee@outlook.fr>
2023-12-01 15:51:43 +0100

    Setting up

--- run.py
+++
@@ -1,6 +0,0 @@
-from adventofcode.y2021.day15.solution import Solution
-
-if __name__ == "__main__":
-    s = Solution(example=False)
-    print(f"Part 1: {s.part1()}")
-    print(f"Part 2: {s.part2()}")
