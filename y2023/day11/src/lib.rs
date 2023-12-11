04c8faf4e0329c9d093e4b3ce271e4aa7aaac056
Tim024 <14969703+Tim024@users.noreply.github.com>
2023-12-11 16:44:59 +0100

    day11

--- y2023/day11/src/lib.rs
+++ y2023/day11/src/lib.rs
@@ -10,8 +10,7 @@ fn abs_difference(x: usize, y: usize) -> u32 {
 }
 
 #[derive(Debug, Clone, Copy)]
-struct Galaxy
-{
+struct Galaxy {
     x: usize,
     y: usize,
 }
@@ -20,8 +19,7 @@ fn manhattan(p1: Galaxy, p2: Galaxy) -> u32 {
     abs_difference(p2.x, p1.x) + abs_difference(p2.y, p1.y)
 }
 
-fn expand_space(data: Vec<Vec<char>>) -> Vec<Vec<char>> {
-    let mut new_data = data.clone();
+fn empty_spaces(data: Vec<Vec<char>>) -> (Vec<usize>, Vec<usize>) {
     let mut new_rows_index = Vec::new();
     let mut new_cols_index = Vec::new();
     for (i, row) in data.iter().enumerate() {
@@ -29,11 +27,18 @@ fn expand_space(data: Vec<Vec<char>>) -> Vec<Vec<char>> {
             new_rows_index.push(i);
         }
     }
-    for (i, col) in data[0].iter().enumerate() {
+    for (i, _col) in data[0].iter().enumerate() {
         if data.iter().all(|x| x[i] == '.') {
             new_cols_index.push(i);
         }
     }
+
+    return (new_rows_index, new_cols_index);
+}
+
+fn expand_space(data: Vec<Vec<char>>) -> Vec<Vec<char>> {
+    let mut new_data = data.clone();
+    let (new_rows_index, new_cols_index) = empty_spaces(data.clone());
     for i in new_rows_index.iter().rev() {
         let empty_row = vec!['.'; data[0].len()];
         new_data.insert(*i, empty_row)
@@ -46,66 +51,98 @@ fn expand_space(data: Vec<Vec<char>>) -> Vec<Vec<char>> {
     new_data
 }
 
-
-
-pub fn part1() -> String{
+pub fn part1() -> String {
     let data = read_data_matrix("./day11/src/data.input");
 
     let data = expand_space(data);
 
-    let galaxy_positions: Vec<Galaxy> = data.iter().enumerate().flat_map(|(x, row)| {
-        row.iter().enumerate().filter_map(move |(y, col)| {
-            if *col == '#' {
-                Some(Galaxy{x, y})
-            } else {
-                None
-            }
+    let galaxy_positions: Vec<Galaxy> = data
+        .iter()
+        .enumerate()
+        .flat_map(|(x, row)| {
+            row.iter().enumerate().filter_map(move |(y, col)| {
+                if *col == '#' {
+                    Some(Galaxy { x, y })
+                } else {
+                    None
+                }
+            })
         })
-    }).collect();
+        .collect();
 
     let mut distances = Vec::new();
-    for (i,g1) in galaxy_positions.iter().enumerate() {
-        for (j,g2) in galaxy_positions.iter().enumerate() {
+    for (i, g1) in galaxy_positions.iter().enumerate() {
+        for (j, g2) in galaxy_positions.iter().enumerate() {
             if j <= i {
                 continue;
             }
             distances.push(manhattan(*g1, *g2));
-            println!("G1 {i}: {g1:?}, G2 {j}: {g2:?}, Distance: {}", manhattan(*g1, *g2));
+            // println!(
+            //     "G1 {i}: {g1:?}, G2 {j}: {g2:?}, Distance: {}",
+            //     manhattan(*g1, *g2)
+            // );
         }
     }
 
     write_data_matrix("./day11/src/data.output", &data);
     let sum_distances: u32 = distances.iter().sum();
 
-    return sum_distances.to_string()
+    return sum_distances.to_string();
 }
 
-pub fn part2() -> String{
+
+
+fn manhattan_p2(p1: Galaxy, p2: Galaxy, new_rows_index: Vec<u64>, new_cols_index: Vec<u64>) -> u64 {
+    let mut x1 = p1.x as u64;
+    let mut x2 = p2.x as u64;
+    if p1.x > p2.x {
+        x1 = p2.x as u64;
+        x2 = p1.x as u64;
+    }
+    let mut y1 = p1.y as u64;
+    let mut y2 = p2.y as u64;
+    if p1.y > p2.y {
+        y1 = p2.y as u64;
+        y2 = p1.y as u64;
+    }
+    let n_empty_rows = new_rows_index.iter().filter(|x| **x > x1).filter(|x| **x < x2).count();
+    let n_empty_cols = new_cols_index.iter().filter(|x| **x > y1).filter(|x| **x < y2).count();
+
+    return ((x2 - x1) + (y2 - y1) + (n_empty_rows  as u64 + n_empty_cols  as u64)*999_999) as u64;
+}
+
+pub fn part2() -> String {
     let data = read_data_matrix("./day11/src/data.input");
 
-    let galaxy_positions: Vec<Galaxy> = data.iter().enumerate().flat_map(|(x, row)| {
-        row.iter().enumerate().filter_map(move |(y, col)| {
-            if *col == '#' {
-                Some(Galaxy{x, y})
-            } else {
-                None
-            }
+    let (new_rows_index, new_cols_index) = empty_spaces(data.clone());
+    let new_rows_index:Vec<u64> = new_rows_index.iter().map(|x| *x as u64).collect();
+    let new_cols_index:Vec<u64> = new_cols_index.iter().map(|x| *x as u64).collect();
+
+
+    let galaxy_positions: Vec<Galaxy> = data
+        .iter()
+        .enumerate()
+        .flat_map(|(x, row)| {
+            row.iter().enumerate().filter_map(move |(y, col)| {
+                if *col == '#' {
+                    Some(Galaxy { x, y })
+                } else {
+                    None
+                }
+            })
         })
-    }).collect();
+        .collect();
 
     let mut distances = Vec::new();
-    for (i,g1) in galaxy_positions.iter().enumerate() {
-        for (j,g2) in galaxy_positions.iter().enumerate() {
+    for (i, g1) in galaxy_positions.iter().enumerate() {
+        for (j, g2) in galaxy_positions.iter().enumerate() {
             if j <= i {
                 continue;
             }
-            distances.push(manhattan(*g1, *g2)); // Change the way to calculate distance
-            println!("G1 {i}: {g1:?}, G2 {j}: {g2:?}, Distance: {}", manhattan(*g1, *g2));
+            distances.push(manhattan_p2(*g1, *g2, new_rows_index.clone(), new_cols_index.clone()));
         }
     }
+    let sum_distances: u64 = distances.iter().sum();
 
-    write_data_matrix("./day11/src/data.output", &data);
-    let sum_distances: u32 = distances.iter().sum();
-
-    return sum_distances.to_string()
+    return sum_distances.to_string();
 }
